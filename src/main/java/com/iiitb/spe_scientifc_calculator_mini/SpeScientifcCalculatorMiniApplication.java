a5d77a0ca1b748473abd3f7f50f3c917993626bd
amitkumar <amitkumar.chottu58@gmail.com>
2022-04-10 23:30:20 +0530

    Initial commit

---
+++ src/main/java/com/iiitb/spe_scientifc_calculator_mini/SpeScientifcCalculatorMiniApplication.java
@@ -0,0 +1,13 @@
+package com.iiitb.spe_scientifc_calculator_mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeScientifcCalculatorMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeScientifcCalculatorMiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iiitb/spe_scientifc_calculator_mini/SpeScientifcCalculatorMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.spe_scientifc_calculator_mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeScientifcCalculatorMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
